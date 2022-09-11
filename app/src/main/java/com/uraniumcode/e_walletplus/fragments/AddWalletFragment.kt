58e9657c69d722373e6ca9dbf16a680cd3d44661
cagdas-kaya <55627762+cagdas-kaya@users.noreply.github.com>
2022-09-11 04:58:13 +0300

    start project and added fragments

---
+++ app/src/main/java/com/uraniumcode/e_walletplus/fragments/AddWalletFragment.kt
@@ -0,0 +1,29 @@
+package com.uraniumcode.e_walletplus.fragments
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.google.android.material.bottomsheet.BottomSheetDialogFragment
+import com.uraniumcode.e_walletplus.R
+
+class AddWalletFragment : BottomSheetDialogFragment() {
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setStyle(BottomSheetDialogFragment.STYLE_NORMAL, R.style.BottomSheetTransparentTheme)
+
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_add_wallet, container, false)
+    }
+
+
+}
