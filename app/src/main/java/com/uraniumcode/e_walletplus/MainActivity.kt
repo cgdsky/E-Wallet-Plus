ee8d810a42b0406b1db3cc0b6e54bfbadfa90f08
cagdas-kaya <55627762+cagdas-kaya@users.noreply.github.com>
2022-10-25 01:55:59 +0300

    add first load tutorial animate

--- app/src/main/java/com/uraniumcode/e_walletplus/MainActivity.kt
+++ app/src/main/java/com/uraniumcode/e_walletplus/MainActivity.kt
@@ -1,10 +1,15 @@
 package com.uraniumcode.e_walletplus
 
+import android.animation.ObjectAnimator
+import android.annotation.SuppressLint
+import android.content.SharedPreferences
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.view.animation.Animation
 import androidx.navigation.Navigation
 import com.uraniumcode.e_walletplus.fragments.HomeFragmentDirections
+import com.uraniumcode.e_walletplus.utils.Constants
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -12,6 +17,7 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         listeners()
+        controlWalletTouchAnimate()
     }
 
     private fun listeners(){
@@ -19,8 +25,26 @@ class MainActivity : AppCompatActivity() {
                 onBackPressed()
         }
         img_add_wallet.setOnClickListener {
+            controlWalletTouchAnimate()
             val action = HomeFragmentDirections.actionHomeFragmentToAddWalletFragment()
             Navigation.findNavController(fragment_container_view).navigate(action)
+        }
+    }
+
+    @SuppressLint("WrongConstant")
+    private fun controlWalletTouchAnimate(){
+        val prefs: SharedPreferences = application.getSharedPreferences(Constants().PREF_NAME, MODE_PRIVATE)
+        val objAnimator = ObjectAnimator.ofFloat(img_touch, "alpha", 0f, 1f)
+
+        if (prefs.getBoolean(Constants().PREF_FIRST_TIME, true)) {
+        img_touch.visibility = View.VISIBLE
+        objAnimator.duration = 1000
+        objAnimator.repeatMode = Animation.REVERSE
+        objAnimator.repeatCount = Animation.INFINITE
+        objAnimator.start()
+        }else{
+            img_touch.visibility = View.GONE
+            objAnimator.removeAllListeners()
         }
     }
 
@@ -37,5 +61,7 @@ class MainActivity : AppCompatActivity() {
             img_add_wallet.visibility = View.VISIBLE
         }
     }
+
+
 
 }
